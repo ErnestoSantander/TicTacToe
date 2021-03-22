011d3803b6c409cfc8f81a7039602ffcb54c7a43
nesto <ernesto.santanderlpz@gmail.com>
2021-03-22 15:07:04 -0600

    Completing game functionallity

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ function Square(props) {
 
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O' ;
         this.setState({
           squares:squares,
