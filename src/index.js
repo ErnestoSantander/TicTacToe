568319baf3781759aff1712a9597757d891109e9
nesto <ernesto.santanderlpz@gmail.com>
2021-03-22 14:08:19 -0600

    First commit play game

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import './index.css';
 
 //Componente de funcion Square que solo renderiza 
 function Square(props) {
-    render() {
       return (
         <button className="square" onClick={props.onClick}>
           {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
@@ -18,26 +16,38 @@ function Square(props) {
             super(props);
             this.state = {
                 squares: Array(9).fill(null),
+                xIsNext: true,
             }
 
         }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares:squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O' ;
+        this.setState({
+          squares:squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
-      return <Square 
+      return <Square
             value={this.state.squares[i]} 
             onClick={() => this.handleClick(i)}
 
       />;
     }
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: '+ winner;
+        
+      } else {
+        status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+        
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -84,3 +94,22 @@ function Square(props) {
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
